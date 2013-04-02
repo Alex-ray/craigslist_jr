9ff41ba89890e07638d99a4cff46c673201cfa76
Apprentice <apprentice@devbootcamp.com>
2013-04-02 10:33:53 -0700

    table migration

--- db/migrate/20130402102625_create_categories.rb
+++ db/migrate/20130402102625_create_categories.rb
@@ -1,4 +1,8 @@
 class CreateCategories < ActiveRecord::Migration
   def change
+    create_table :categories do |c|
+      c.string :name
+      c.timestamps
+    end
   end
 end
--- db/migrate/20130402102630_create_posts.rb
+++ db/migrate/20130402102630_create_posts.rb
@@ -1,4 +1,13 @@
 class CreatePosts < ActiveRecord::Migration
   def change
+    create_table :posts do |p|
+      p.string :title
+      p.text :description
+      p.integer :price
+      p.string :email
+      p.string :secret_key
+      p.integer :category_id
+      p.timestamps
+    end
   end
 end
